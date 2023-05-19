217868918b925cfa8b8c0f8f8e68ae7e96427246
Smurflo-k <svd112002@gmail.com>
2023-05-19 15:47:09 +0300

    Done

---
+++ RVA_Sol/RVA_Sol/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace RVA_Sol
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.lblEncryptedData = new System.Windows.Forms.Label();
+      this.lblDecryptedData = new System.Windows.Forms.Label();
+      this.btnCheckIntegrity = new System.Windows.Forms.Button();
+      this.txtInputData = new System.Windows.Forms.TextBox();
+      this.lblIntegrity = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // lblEncryptedData
+      // 
+      this.lblEncryptedData.AutoSize = true;
+      this.lblEncryptedData.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.lblEncryptedData.Location = new System.Drawing.Point(551, 50);
+      this.lblEncryptedData.Name = "lblEncryptedData";
+      this.lblEncryptedData.Size = new System.Drawing.Size(0, 27);
+      this.lblEncryptedData.TabIndex = 0;
+      // 
+      // lblDecryptedData
+      // 
+      this.lblDecryptedData.AutoSize = true;
+      this.lblDecryptedData.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.lblDecryptedData.Location = new System.Drawing.Point(551, 157);
+      this.lblDecryptedData.Name = "lblDecryptedData";
+      this.lblDecryptedData.Size = new System.Drawing.Size(0, 27);
+      this.lblDecryptedData.TabIndex = 1;
+      // 
+      // btnCheckIntegrity
+      // 
+      this.btnCheckIntegrity.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.btnCheckIntegrity.Location = new System.Drawing.Point(523, 560);
+      this.btnCheckIntegrity.Name = "btnCheckIntegrity";
+      this.btnCheckIntegrity.Size = new System.Drawing.Size(135, 53);
+      this.btnCheckIntegrity.TabIndex = 2;
+      this.btnCheckIntegrity.Text = "button1";
+      this.btnCheckIntegrity.UseVisualStyleBackColor = true;
+      this.btnCheckIntegrity.Click += new System.EventHandler(this.btnCheckIntegrity_Click);
+      // 
+      // txtInputData
+      // 
+      this.txtInputData.Location = new System.Drawing.Point(540, 405);
+      this.txtInputData.Name = "txtInputData";
+      this.txtInputData.Size = new System.Drawing.Size(100, 22);
+      this.txtInputData.TabIndex = 3;
+      // 
+      // lblIntegrity
+      // 
+      this.lblIntegrity.AutoSize = true;
+      this.lblIntegrity.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+      this.lblIntegrity.Location = new System.Drawing.Point(551, 252);
+      this.lblIntegrity.Name = "lblIntegrity";
+      this.lblIntegrity.Size = new System.Drawing.Size(0, 27);
+      this.lblIntegrity.TabIndex = 4;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1169, 666);
+      this.Controls.Add(this.lblIntegrity);
+      this.Controls.Add(this.txtInputData);
+      this.Controls.Add(this.btnCheckIntegrity);
+      this.Controls.Add(this.lblDecryptedData);
+      this.Controls.Add(this.lblEncryptedData);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label lblEncryptedData;
+    private System.Windows.Forms.Label lblDecryptedData;
+    private System.Windows.Forms.Button btnCheckIntegrity;
+    private System.Windows.Forms.TextBox txtInputData;
+    private System.Windows.Forms.Label lblIntegrity;
+  }
+}
+
---
+++ RVA_Sol/RVA_Sol/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace RVA_Sol
+{
+  public partial class Form1 : Form
+  {
+    private BigInteger publicKey = 65537; // Задайте свое значение открытого ключа
+    private BigInteger privateKey = 123456789; // Задайте свое значение закрытого ключа
+
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void btnCheckIntegrity_Click(object sender, EventArgs e)
+    {
+      // Получаем входные данные из текстового поля
+      string inputData = txtInputData.Text;
+
+      // Преобразуем входные данные в байтовый массив
+      byte[] inputBytes = Encoding.UTF8.GetBytes(inputData);
+
+      // Создаем экземпляр класса BigInteger из входных данных
+      BigInteger inputNumber = new BigInteger(inputBytes);
+
+      // Шифруем данные с использованием открытого ключа
+      BigInteger encryptedNumber = BigInteger.ModPow(inputNumber, publicKey, privateKey);
+
+      // Дешифруем данные с использованием закрытого ключа
+      BigInteger decryptedNumber = BigInteger.ModPow(encryptedNumber, privateKey, publicKey);
+
+      // Преобразуем результат дешифрования в байтовый массив
+      byte[] decryptedBytes = decryptedNumber.ToByteArray();
+
+      // Преобразуем байтовый массив в строку
+      string decryptedData = Encoding.UTF8.GetString(decryptedBytes);
+
+      // Выводим результаты
+      lblEncryptedData.Text = "Зашифрованные данные: " + encryptedNumber.ToString();
+      lblDecryptedData.Text = "Расшифрованные данные: " + decryptedData;
+
+      // Проверяем целостность данных
+      bool isIntegrityPreserved = inputData.Equals(decryptedData);
+      lblIntegrity.Text = "Целостность данных: " + isIntegrityPreserved.ToString();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
